fe716c4e13f3632b4ecd8019742b3af26f5d6509
sarojmaharjan99999 <sarojmhrzn@gmail.com>
2016-09-05 18:03:03 +0545

    Create custoerms get API to fetch all customers

--- nea_app/app.js
+++ nea_app/app.js
@@ -6,9 +6,20 @@ var app     = express();
 
 var port = 5000;
 
+var models = require('./models/index');
 
 app.get('/',function (req,res) {
     res.json({message:"hello world"});
+});
+
+app.get('/customers',function (req,res) {
+    models.Customer.findAll()
+        .then(function (customers) {
+            res.json({customer:customers})
+        })
+        .catch(function (err) {
+            res.json({error:err});
+        })
 });
 
 app.listen(port ,function () {
