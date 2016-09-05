dbcb91b5b1ee349dd190dd34e4af618adb376f45
sarojmaharjan99999 <sarojmhrzn@gmail.com>
2016-09-05 17:26:14 +0545

    Create model Customer

---
+++ nea_app/models/customer.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Customer = sequelize.define('Customer', {
+    name: DataTypes.STRING,
+    due_amount: DataTypes.FLOAT
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return Customer;
+};
